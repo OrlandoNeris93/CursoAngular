7a068ec31446cb263788df77e4f78ef9471a8665
Orlando Neris <54051581+OrlandoNeris93@users.noreply.github.com>
2024-10-18 21:14:38 -0300

    Clase 23 - Planteo de Enrutamiento

--- routing/src/main.ts
+++ routing/src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from './app/home/home.component';
+import { ContactComponent } from './app/contact/contact.component';
+import { AboutComponent } from './app/about/about.component';
+import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
